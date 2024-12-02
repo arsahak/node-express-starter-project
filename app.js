aaebffe1d200a7a665f5cd7024fd1a6f761593c2
arsahak <arsahak99@gmail.com>
2024-12-02 21:46:57 +0600

    update

--- app.js
+++ app.js
@@ -1,47 +1,47 @@
-const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const createError = require("http-errors");
-const xssClean = require("xss-clean");
-const cookieParser = require("cookie-parser");
-const rateLimit = require("express-rate-limit");
-const { userRouter } = require("./routers/userRouter");
-const { seedRouter } = require("./routers/seedRouter");
-const { authRouter } = require("./routers/authRouter");
-const { errorResponse } = require("./controllers/responseController");
+// const express = require("express");
+// const morgan = require("morgan");
+// const bodyParser = require("body-parser");
+// const createError = require("http-errors");
+// const xssClean = require("xss-clean");
+// const cookieParser = require("cookie-parser");
+// const rateLimit = require("express-rate-limit");
+// const { userRouter } = require("./routers/userRouter");
+// const { seedRouter } = require("./routers/seedRouter");
+// const { authRouter } = require("./routers/authRouter");
+// const { errorResponse } = require("./controllers/responseController");
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  limit: 100,
-  message: "Too many reqeust from this ip please try later",
-  // standardHeaders: "draft-7",
-  // legacyHeaders: false,
-});
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   limit: 100,
+//   message: "Too many reqeust from this ip please try later",
+//   // standardHeaders: "draft-7",
+//   // legacyHeaders: false,
+// });
 
-const app = express();
+// const app = express();
 
-app.use(cookieParser());
-app.use(limiter);
-app.use(xssClean());
-app.use(morgan("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use(limiter);
+// app.use(xssClean());
+// app.use(morgan("dev"));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use("/api", authRouter);
-app.use("/api/seed", seedRouter);
-app.use("/api", userRouter);
+// app.use("/api", authRouter);
+// app.use("/api/seed", seedRouter);
+// app.use("/api", userRouter);
 
-//client error handling
+// //client error handling
 
-app.use((req, res, next) => {
-  next(createError(404, "route not found"));
-});
+// app.use((req, res, next) => {
+//   next(createError(404, "route not found"));
+// });
 
-//server error handling
+// //server error handling
 
-app.use((err, req, res, next) => {
-  return errorResponse(res, { statusCode: err.status, message: err.message });
-});
+// app.use((err, req, res, next) => {
+//   return errorResponse(res, { statusCode: err.status, message: err.message });
+// });
 
-module.exports = app;
+// module.exports = app;
