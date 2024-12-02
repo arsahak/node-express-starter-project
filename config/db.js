164a6df7faae9e256fc975e9af29eb248f0f3e5f
arsahak <arsahak99@gmail.com>
2024-12-03 00:01:23 +0600

    database update

--- config/db.js
+++ config/db.js
@@ -3,18 +3,32 @@ const logger = require("../controllers/loggerController");
 const { dataBaseUrl } = require("../secret");
 require("dotenv").config();
 
-const connectDatabase = async (options = {}) => {
-  try {
-    await mongoose.connect( process.env.DB_URL, options);
-    console.log("connect to DB is successfully established");
-    // logger.log("info", "connect to DB is successfully established")
+// const connectDatabase = async (options = {}) => {
+//   try {
+//     await mongoose.connect( process.env.DB_URL, options);
+//     console.log("connect to DB is successfully established");
+//     // logger.log("info", "connect to DB is successfully established")
 
-    mongoose.connection.on("error", (error) => {
-      console.error("DB connect error", error);
-    });
-  } catch (error) {
-    console.error("could not connect to DB", error.toString());
-  }
-};
+//     mongoose.connection.on("error", (error) => {
+//       console.error("DB connect error", error);
+//     });
+//   } catch (error) {
+//     console.error("could not connect to DB", error.toString());
+//   }
+// };
 
-module.exports = connectDatabase;
+// module.exports = connectDatabase;
+
+// const mongoose = require('mongoose');
+
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log('mongodb atlas is connected');
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
+
