7a9f0842aa34aebfdbf845c761296ce5bbadce38
arsahak <arsahak99@gmail.com>
2024-12-03 21:33:33 +0600

    update

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const { userRouter } = require("./routers/userRouter");
 const { seedRouter } = require("./routers/seedRouter");
 const { authRouter } = require("./routers/authRouter");
 const { errorResponse } = require("./controllers/responseController");
+const connectDatabase = require("./config/db");
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -40,6 +41,8 @@ app.get("/", (req, res) => {
 });
 
 //client error handling
+
+connectDatabase()
 
 app.use((req, res, next) => {
   next(createError(404, "route not found"));
