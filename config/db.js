db46744e553a12f12952bc17efb6d800f3b8be55
arsahak <arsahak99@gmail.com>
2024-12-03 21:46:57 +0600

    update

--- config/db.js
+++ config/db.js
@@ -2,22 +2,18 @@ const mongoose = require("mongoose");
 const logger = require("../controllers/loggerController");
 const { dataBaseUrl } = require("../secret");
 
-let isConnected = false; // Track connection state
 
 const connectDatabase = async (options = {}) => {
-  if (isConnected) {
-    logger.log("info", "Using existing database connection");
-    return;
-  }
   try {
-    const db = await mongoose.connect(dataBaseUrl, options);
-    isConnected = db.connections[0].readyState === 1; // Check connection state
-    logger.log("info", "Connected to DB successfully");
+    await mongoose.connect(dataBaseUrl, options);
+    // console.log("connect to DB is successfully established");
+    logger.log("info", "connect to DB is successfully established")
+
     mongoose.connection.on("error", (error) => {
       console.error("DB connect error", error);
     });
   } catch (error) {
-    console.error("Could not connect to DB", error.toString());
+    console.error("could not connect to DB", error.toString());
   }
 };
 
--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const app = require("./app");
 const connectDatabase = require("./config/db");
 const { serverPort } = require("./secret");
 
-(async () => {
-  await connectDatabase();
-})();
+ connectDatabase();
+
 
 app.listen(serverPort, async () => {
   console.log(`Server is running at http://localhost:8000`);
