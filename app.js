21524054b02784186b85b072fc69544653296d1f
arsahak <arsahak99@gmail.com>
2024-12-03 22:21:42 +0600

    update

--- app.js
+++ app.js
@@ -1,38 +1,63 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require('cors')
-const bodyParser = require("body-parser");
-const createError = require("http-errors");
-const xssClean = require("xss-clean");
-const cookieParser = require("cookie-parser");
-const rateLimit = require("express-rate-limit");
-const { userRouter } = require("./routers/userRouter");
-const { seedRouter } = require("./routers/seedRouter");
-const { authRouter } = require("./routers/authRouter");
-const { errorResponse } = require("./controllers/responseController");
+// const express = require("express");
+// const morgan = require("morgan");
+// const cors = require('cors')
+// const bodyParser = require("body-parser");
+// const createError = require("http-errors");
+// const xssClean = require("xss-clean");
+// const cookieParser = require("cookie-parser");
+// const rateLimit = require("express-rate-limit");
+// const { userRouter } = require("./routers/userRouter");
+// const { seedRouter } = require("./routers/seedRouter");
+// const { authRouter } = require("./routers/authRouter");
+// const { errorResponse } = require("./controllers/responseController");
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  limit: 100,
-  message: "Too many reqeust from this ip please try later",
-  // standardHeaders: "draft-7",
-  // legacyHeaders: false,
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   limit: 100,
+//   message: "Too many reqeust from this ip please try later",
+//   // standardHeaders: "draft-7",
+//   // legacyHeaders: false,
+// });
+
+// const app = express();
+
+// app.use(cookieParser());
+// app.use(limiter);
+// app.use(cors())
+// app.use(xssClean());
+// app.use(morgan("dev"));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const express = require("express");
+const createError = require("http-errors");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { seedRouter } = require("./routers/seedRouter");
+
+
+app.use(
+  cors({
+    origin: ["https://eshop-tutorial-pyri.vercel.app", "http://localhost:3000"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/test", (req, res) => {
+  res.send("Hello world!");
 });
 
-const app = express();
-
-app.use(cookieParser());
-app.use(limiter);
-app.use(cors())
-app.use(xssClean());
-app.use(morgan("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 
-app.use("/api", authRouter);
+// app.use("/api", authRouter);
 app.use("/api/seed", seedRouter);
-app.use("/api", userRouter);
+// app.use("/api", userRouter);
 
 
 app.get("/", (req, res) => {
--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const app = require("./app");
 const connectDatabase = require("./config/db");
 const { serverPort } = require("./secret");
 
+
+
 connectDatabase();
 
 app.listen(serverPort, async () => {
   console.log(`Server is running at http://localhost:8000`);
-
 });
 
