7bb9b6e55428a1eb9a95023501110bf4e3c036c8
arsahak <arsahak99@gmail.com>
2024-12-03 21:50:01 +0600

    update

--- app.js
+++ app.js
@@ -24,9 +24,15 @@ const limiter = rateLimit({
 
 const app = express();
 
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 app.use(limiter);
-app.use(cors())
 app.use(xssClean());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const app = require("./app");
 const connectDatabase = require("./config/db");
 const { serverPort } = require("./secret");
 
- connectDatabase();
 
 
 app.listen(serverPort, async () => {
   console.log(`Server is running at http://localhost:8000`);
+  await connectDatabase();
 });
 
