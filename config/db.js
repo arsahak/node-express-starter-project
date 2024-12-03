87e8d351a9271640d68ca6cad138f8811a7c298c
arsahak <arsahak99@gmail.com>
2024-12-03 23:53:27 +0600

    upadte value

--- config/db.js
+++ config/db.js
@@ -3,30 +3,12 @@ const logger = require("../controllers/loggerController");
 const { dataBaseUrl } = require("../secret");
 
 
-// const connectDatabase = async (options = {}) => {
-//   try {
-//     await mongoose.connect(dataBaseUrl, options);
-//     // console.log("connect to DB is successfully established");
-//     logger.log("info", "connect to DB is successfully established")
-
-//     mongoose.connection.on("error", (error) => {
-//       console.error("DB connect error", error);
-//     });
-//   } catch (error) {
-//     console.error("could not connect to DB", error.toString());
-//   }
-// };
-
-
-const connectDatabase = () => {
-  mongoose
-    .connect(dataBaseUrl , {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then((data) => {
-      console.log(`mongod connected with server: ${data.connection.host}`);
-    });
-};
-
-module.exports = connectDatabase;
+mongoose
+  .connect(dataBaseUrl)
+  .then(() => {
+    console.log('mongodb atlas is connected');
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const app = require("./app");
-const connectDatabase = require("./config/db");
 const { serverPort } = require("./secret");
-
-
-
-connectDatabase();
 
 app.listen(serverPort, async () => {
   console.log(`Server is running at http://localhost:8000`);
+
 });
 
